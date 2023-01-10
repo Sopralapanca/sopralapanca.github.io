cf5141156d5a7dea173fb552b1417f10c515c1e5
Sopralapanca <giacomo.carfi@outlook.it>
2023-01-10 17:15:53 +0100

    added checkbox to fix

--- refresh_page.js
+++ refresh_page.js
@@ -1,17 +1,33 @@
-document.write(`<frameset><frame src="${location.href}"></frameset>`);
+var containerDiv = document.querySelectorAll('.container')[0];
+var checkbox = document.createElement('input');
+checkbox.type = "checkbox";
+checkbox.name = "refresh-checkbox";
+checkbox.value = "value";
+checkbox.id = "id";
 
-setInterval(async () => {
+var label = document.createElement('label');
+label.htmlFor = "id";
+
+label.appendChild(document.createTextNode('Autorefresh'));
+
+containerDiv.appendChild(checkbox);
+containerDiv.appendChild(label);
+
+document.write(`<frameset><frame src="${location.href}"></frameset>`);
+setInterval( () => {
         const doc = window.frames[0].document;
-        const button = doc.querySelectorAll('.button')[0];
+
+
+        /*const button = doc.querySelectorAll('.button')[0];
         if (button !== undefined) {
             const audio = new Audio("https://github.com/Sopralapanca/sopralapanca.github.io/blob/master/changes-cut.mp3?raw=true");
             audio.play();
-            await new Promise(r => setTimeout(r, 6500));
+            new Promise(r => setTimeout(r, 6500));
             button.click();
-        }
+        }*/
 
 
         document.getElementsByTagName('frame')[0].src += ''
 
     },
-    61000);
+    5000);
