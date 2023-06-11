568e97846a605639a32009d17f3bae2c4e770664
Sopralapanca <giacomo.carfi@outlook.it>
2023-06-11 18:41:03 +0200

    first implementation refresh script

---
+++ refresh_bookmarklet.js
@@ -0,0 +1,12 @@
+javascript: (function() {
+    var addScript=function(filename){
+        var e=document.createElement('script');
+        e.type='text/javascript';
+        e.src=filename;
+        if(typeof(e)!=='undefined'){
+            document.getElementsByTagName('head')[0].appendChild(e);
+        }
+    };
+    addScript('https://sopralapanca.github.io/refresh_page.js');
+})();
+
--- refresh_page.js
+++ refresh_page.js
@@ -11,57 +11,24 @@ label.htmlFor = "id";
 label.appendChild(document.createTextNode('Autorefresh'));
 
 containerDiv.appendChild(checkbox);
-containerDiv.appendChild(label);
+containerDiv.appendChild(label);*/
 
 document.write(`<frameset><frame src="${location.href}"></frameset>`);
-setInterval( () => {
+intervalID  = setInterval( () => {
         const doc = window.frames[0].document;
 
         const button = doc.querySelectorAll('.button')[0];
         if (button !== undefined) {
             const audio = new Audio("https://github.com/Sopralapanca/sopralapanca.github.io/blob/master/changes-cut.mp3?raw=true");
             audio.play();
-            new Promise(r => setTimeout(r, 6500));
-            button.click();
-        }
+            setTimeout(() => {
+                const href = button.getAttribute('href');
+                window.open(href, '_blank');
 
+            }, 6500);
+            clearInterval(intervalID);
+        }
 
         document.getElementsByTagName('frame')[0].src += ''
 
-    },
-    5000);
-
-*/
-
-// Define the function to check if the specific button is present
-function isNRTPresent() {
-    var container = document.getElementsByClassName('container')[0];
-    if (container.innerText.includes('No tasks are currently available')) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-// Define the function to refresh the page content and check for the button
-function refreshAndCheckButton() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            var parser = new DOMParser();
-            var responseDoc = parser.parseFromString(this.responseText, 'text/html');
-            if(isNRTPresent()) {
-                console.log('NRT is present');
-                setTimeout(refreshAndCheckButton, 60000);
-            } else {
-                console.log('task found');
-            }
-        }
-    };
-    xhttp.open('GET', window.location.href, true);
-    xhttp.send();
-}
-
-// Call the function to start refreshing
-refreshAndCheckButton();
+    },60000);
