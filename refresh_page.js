0aa8752405ebe90dfeab5f9aab7a000fd55cd603
Sopralapanca <giacomo.carfi@outlook.it>
2023-06-13 16:39:55 +0200

    refresh version 1.1.0 working open taks in a new page, close this page message added

--- refresh_page.js
+++ refresh_page.js
@@ -6,36 +6,37 @@ iframeElement.style.border = 'none';
 
 document.documentElement.innerHTML = '';
 document.documentElement.appendChild(iframeElement);
+document.title = 'Rater Hub';
 
-console.log('version 1.0.2');
+console.log('version 1.0.0');
 
 intervalID  = setInterval( () => {
     console.log('checking for button');
-    document.documentElement.innerHTML += '';
 
-    let button = undefined;
-    setTimeout(() => {
-        const doc = window.frames[0].document;
-        button = doc.querySelectorAll('.button')[0];
-    }, 2000);
+    const doc = window.frames[0].document;
+    const button = doc.querySelectorAll('.button')[0];
 
     if (button !== undefined) {
-        console.log('button found');
         const audio = new Audio("https://github.com/Sopralapanca/sopralapanca.github.io/blob/master/changes-cut.mp3?raw=true");
-        let href = button.getAttribute('href');
+        const href = button.getAttribute('href');
 
         audio.play()
-            .then(() => {
-                console.log('button found');
-            })
-            .catch((error) => {
-                console.error('Error playing audio:', error);
-            });
+                .then(() => {
+                    console.log('button found');
+                })
+                .catch((error) => {
+                    console.error('Error playing audio:', error);
+                });
 
         audio.addEventListener('ended', function() {
             window.open(href, '_blank');
+            // change content of current page and insert "CLOSE THIS PAGE" message
+            document.documentElement.innerHTML = '<h1 style="text-align: center; margin-top: 50px;">CLOSE THIS PAGE</h1>';
         });
 
         clearInterval(intervalID);
     }
-},61500);
+
+    document.documentElement.innerHTML += '';
+
+},5000);
