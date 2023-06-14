a23b982d0791669bece19980020a733763da2fa0
Sopralapanca <giacomo.carfi@outlook.it>
2023-06-14 23:42:03 +0200

    multiple buttons added to refresh page

--- refresh_page.js
+++ refresh_page.js
@@ -8,17 +8,20 @@ document.documentElement.innerHTML = '';
 document.documentElement.appendChild(iframeElement);
 document.title = 'Rater Hub';
 
+
 console.log('version 1.0.0');
 
 intervalID  = setInterval( () => {
     console.log('checking for button');
 
     const doc = window.frames[0].document;
-    const button = doc.querySelectorAll('.button')[0];
+    const buttons = doc.querySelectorAll('.button');
+    let adultButtons = [];
+    let audioButtons = [];
+    let restButtons = [];
 
-    if (button !== undefined) {
+    if(buttons.length !== 0) {
         const audio = new Audio("https://github.com/Sopralapanca/sopralapanca.github.io/blob/master/changes-cut.mp3?raw=true");
-        const href = button.getAttribute('href');
 
         audio.play()
                 .then(() => {
@@ -29,6 +32,17 @@ intervalID  = setInterval( () => {
                 });
 
         audio.addEventListener('ended', function() {
+            for(let button of buttons) {
+                if(button.innerText.toLowerCase().includes("audio")) {
+                    audioButtons.push(button);
+                }else if(button.innerText.toLowerCase().includes("adult") || button.innerText.toLowerCase().includes("offensive")) {
+                    adultButtons.push(button);
+                }else{
+                    restButtons.push(button);
+                }
+            }
+            let allButtons = [...audioButtons, ...adultButtons, ...restButtons];
+            const href = allButtons[0].getAttribute('href');
             window.open(href, '_blank');
             document.documentElement.innerHTML = '<h1 style="text-align: center; margin-top: 50px;">CLOSE THIS PAGE</h1>';
         });
